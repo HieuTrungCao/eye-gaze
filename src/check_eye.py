e7bca13a0fce467c39a269dfef6debe8acc035f0
HieuTrungCao <21021490@vnu.edu.vn>
2023-11-09 09:37:02 +0700

    init project

---
+++ src/check_eye.py
@@ -0,0 +1,13 @@
+
+
+def get_coordinate(frame):
+
+    return (0, 0)
+
+def check_blinking(frame):
+    if True:
+        return "Blinking"
+    elif True:
+        return "Left"
+    
+    return "Right"
