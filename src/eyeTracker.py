e212baad2f5a676e6c281d4b363e2b518fd14ecd
Nguyen Minh Kien <21020638@vnu.edu.vn>
2023-11-26 00:17:53 +0700

    Update data preprocess, fixed pop-up window position

--- src/eyeTracker.py
+++ src/eyeTracker.py
@@ -2,6 +2,11 @@ import cv2
 import mediapipe as mp
 import pyautogui as pag
 from gaze_tracking import GazeTracking
+from screeninfo import get_monitors
+
+def getMainMonitors():
+    m = get_monitors()
+    return m[0]
 
 gaze = GazeTracking()
 
@@ -19,46 +24,47 @@ while True:
     left = gaze.pupil_left_coords()
     right = gaze.pupil_right_coords()
     print(left, right)
-    # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
-    # output = face_mesh.process(rgb_frame)
-    # landmark_points = output.multi_face_landmarks
-    # frame_h, frame_w, _ = frame.shape
-    # results = face_mesh.process(rgb_frame)
+    output = face_mesh.process(rgb_frame)
+    landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+    results = face_mesh.process(rgb_frame)
     
-    # if landmark_points:
-    #     landmarks = landmark_points[0].landmark
-        # for id, landmark in enumerate(landmarks[474:478]):
-        #     x = int(landmark.x * frame_w)
-        #     y = int(landmark.y * frame_h)
-        #     cv2.circle(frame, (x, y), 3, (0, 255, 0))
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+        for id, landmark in enumerate(landmarks[474:478]):
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
             
-        #     screen_x = screen_w * landmark.x
-        #     screen_y = screen_h * landmark.y
-        #     pyautogui.moveTo(screen_x, screen_y)
+            screen_x = screen_w * landmark.x
+            screen_y = screen_h * landmark.y
+            pag.moveTo(screen_x, screen_y)
             
-        # left = [landmarks[145], landmarks[159]]
-        # right = [landmarks[374], landmarks[386]]
+        left = [landmarks[145], landmarks[159]]
+        right = [landmarks[374], landmarks[386]]
 
-        # for landmark in right:
-        #     x = int(landmark.x * frame_w)
-        #     y = int(landmark.y * frame_h)
-        #     cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        for landmark in right:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
         
-        # for landmark in left:
-        #     x = int(landmark.x * frame_w)
-        #     y = int(landmark.y * frame_h)
-        #     cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
 
-        # cv2.circle(frame, (int((left[0].x+left[1].x+right[0].x+right[1].x)* frame_w/4), int((left[0].y+left[1].y+right[0].y+right[1].y) * frame_h/4)), 3, (0, 255, 255))
+        cv2.circle(frame, (int((left[0].x+left[1].x+right[0].x+right[1].x)* frame_w/4), int((left[0].y+left[1].y+right[0].y+right[1].y) * frame_h/4)), 3, (0, 255, 255))
         
-        # pag.moveTo(int((left[0].x+left[1].x+right[0].x+right[1].x)* frame_w/4), int((left[0].y+left[1].y+right[0].y+right[1].y) * frame_h/4))
+        pag.moveTo(int((left[0].x+left[1].x+right[0].x+right[1].x)* frame_w/4), int((left[0].y+left[1].y+right[0].y+right[1].y) * frame_h/4))
         
-        # if (left[0].y - left[1].y) < 0.004:
-        #     pyautogui.click()
-        #     pyautogui.sleep(1)
+        if (left[0].y - left[1].y) < 0.004:
+            pag.click()
+            pag.sleep(1)
             
     cv2.imshow('Eye Controlled Mouse', frame)
+    cv2.moveWindow('Eye Controlled Mouse', (int)((getMainMonitors().width - 1080)/2), (int)((getMainMonitors().height - 720)/2))
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
